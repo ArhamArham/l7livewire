ccb27d9b68d21073850742d28f6a364ba9795e64
ArhamArham <arhamroshan443@gmail.com>
2020-08-20 20:13:10 +0500

    initial release

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    {{$counter}}
+    {{-- keydown=wire:keydown , submit=wire:submit ,click=wire:click--}}
+    <button wire:click="increment">++</button> | <button wire:click="decrement">--</button>
+</div>
---
+++ resources/views/livewire/hello-world.blade.php
@@ -0,0 +1,4 @@
+<div>
+     <input type="text" wire:model.debounce.1000ms="search">
+    <p>{{$search}}</p>
+</div>
